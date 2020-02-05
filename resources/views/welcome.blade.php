872e5f447e1a85d954df658b9d85820c828ee529
Mohamad Jaki Andika 80 <jakiandika19@gmail.com>
2020-02-05 14:53:19 +0700

    Toko_laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,283 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Home</title>
+
+    <style>
+        .navbar,
+        .navbar-light {
+            background: #FFFAF0;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark p-3 bg-dark">
+        <a class="navbar-brand" href="#"><img src="img/toko.jpg" alt="" style="width: 50px; height:40px; border-radius:10px;"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Registar / </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cart</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <nav class="navbar navbar-expand-lg navbar-brand navbar-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse mx-autop" id="navbarNav">
+            <center>
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PRODUK</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CONTACT US</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">ABOUT US</a>
+                    </li>
+                </ul>
+            </center>
+        </div>
+    </nav>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="border-bottom: 2px solid black;">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/slider1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/slider2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/nextar.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <nav class="navbar">
+        <span class="navbar-brand mx-auto
+        ">PRODUK TERBARU</span>
+    </nav>
+
+    <div class="container mb-3 mx-auto">
+        <div class="row">
+            <div class="col-sm">
+                <div class="card mx-auto mb-3" style="width: 14rem;">
+                    <img src="img/rollNroll1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Roll N Roll</h5>
+                        <p class="card-text"> 2 varian rasa milk chocolate dan cookies</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card mx-auto mb-3" style="width: 14rem;">
+                    <img src="img/nabati1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Nabati</h5>
+                        <p class="card-text">dengan varian rasa Richeese dan Richoco</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card mx-auto mb-3" style="width: 14rem;">
+                    <img src="img/pocky2.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Pocky</h5>
+                        <p class="card-text">tersedia dalam aneka rasa, susu cokelat</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card mx-auto mb-3" style="width: 14rem;">
+                    <img src="img/Charm_Safe_night1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Softake</h5>
+                        <p class="card-text">sachet safe night pembalut wanita</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <nav class="navbar" style="border-top: 2px solid black;">
+        <span class="navbar-brand mx-auto
+        ">PRODUK DISKON</span>
+    </nav>
+
+    <div class="container mb-3 mx-auto">
+        <div class="row">
+            <div class="col-sm">
+                <div class="card mx-auto mb-3" style="width: 14rem;">
+                    <img src="img/cheetos1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Cheetos</h5>
+                        <p class="card-text">terasa gurih dan asam-asin di dalamnya</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card mx-auto mb-3" style="width: 14rem;">
+                    <img src="img/doritos1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Doritos</h5>
+                        <p class="card-text">salah satu raksasa makanan di dunia</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card mx-auto mb-3" style="width: 14rem;">
+                    <img src="img/Energen_Coklat1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Energen</h5>
+                        <p class="card-text">sereal yang cocok untuk menghilangkan rasa lapar</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card mx-auto mb-3" style="width: 14rem;">
+                    <img src="img/drinktanggo1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Dring Tanggo</h5>
+                        <p class="card-text">Minuman cokelat yang memiliki kekentalan</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <nav class="navbar" style="border-top: 2px solid black;">
+        <span class="navbar-brand mx-auto
+        ">PRODUK TERLARIS</span>
+    </nav>
+
+    <div class="container mb-3 mx-auto">
+        <div class="row">
+            <div class="col-sm">
+                <div class="card mx-auto mb-3" style="width: 14rem;">
+                    <img src="img/pillows1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Pillows</h5>
+                        <p class="card-text">memiliki rasa yang enak dan lezat</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card mx-auto mb-3" style="width: 14rem;">
+                    <img src="img/rolls1.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Rolls</h5>
+                        <p class="card-text">dengan perpaduan crepes yang renyah</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card mx-auto mb-3" style="width: 14rem;">
+                    <img src="img/saltcheese1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Salt Cheese</h5>
+                        <p class="card-text">tetap menjadi pilihan utama di hati masyarakat</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card mx-auto mb-3" style="width: 14rem;">
+                    <img src="img/sponge1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Sponge</h5>
+                        <p class="card-text">cokelat asli tercium ketika membuka bungkusnya</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="p-3 mb-2 bg-info text-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                        <p><img src="img/toko.jpg" alt="" style="width: 50px; height:40px; border-radius:10px;"> Menjadi jago Coding itu Keren !</p>
+                        <p>Jagocoding.com adalah situs tutorial pemrograman no 1 di Indonesia yang berbasis jejaring sosial (social network). Bagikan tulisanmu ke semua penjuru dunia. Pelajari semua tutorial di sini untuk menjadi seorang yang "jago coding".</p>
+                    </div>
+                    <div class="col-sm">
+                        <p class="navbar bg-info text-white" style="margin-left: 360px;">&copy; 2020 JakiAndika.</p>
+                        <nav class="navbar navbar-expand-lg p-3 mb-2 bg-info">
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" style="color: white;" id="navbarNavAltMarkup">
+                                <div class="navbar-nav ml-auto">
+                                    <a class="nav-item nav-link" href="#">HOME |</a>
+                                    <a class="nav-item nav-link" href="#">PRODUK |</a>
+                                    <a class="nav-item nav-link" href="#">CONTACT US |</a>
+                                    <a class="nav-item nav-link" href="#">ABOUT US</a>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
